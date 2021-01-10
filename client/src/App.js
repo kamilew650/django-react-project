15f1b39d293a31aa8edd3fc409693cdc2821310e
Jarosław Niesyto (jaronie711) <jaronie711@student.polsl.pl>
2021-01-10 02:43:54 +0100

    Get token on first render

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Routes from "./Routes";
 import LoginContext from "./context/LoginContext";
 import TopBar from "./components/TopBar";
 
 function App() {
   const [context, setContext] = useState(null);
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (token) setContext(token);
+  }, []);
 
   return (
     <LoginContext.Provider value={[context, setContext]}>
