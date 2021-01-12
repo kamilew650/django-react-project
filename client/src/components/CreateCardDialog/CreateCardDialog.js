665d5dc5574b7cec5e20085d5da23080be52fcf7
Jarosław Niesyto (jaronie711) <jaronie711@student.polsl.pl>
2021-01-12 21:30:23 +0100

    Create card dialog

--- client/src/components/CreateCardDialog/CreateCardDialog.js
+++ client/src/components/CreateCardDialog/CreateCardDialog.js
@@ -8,6 +8,9 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
 export default function CreateCardDialog(props) {
+  const [toTranslate, setToTranslate] = React.useState("");
+  const [translated, setTranslated] = React.useState("");
+
   const handleClose = () => {
     props.setOpen(false);
   };
@@ -29,13 +32,46 @@ export default function CreateCardDialog(props) {
           id="word"
           label="Word to translate"
           fullWidth
+          value={toTranslate}
+          onChange={(e) => setToTranslate(e.target.value)}
         />
+        <TextField disabled label="Translated" value={translated} fullWidth />
       </DialogContent>
       <DialogActions>
+        <Button
+          onClick={() =>
+            console.log(`
+          Litwo! Ojczyzno maja! Ty jesteś jak zdrowie,
+          Ile cię trzeba cenić, ten tylko się dowie,
+          Kto cię stracił. Dziś piękność twą w całej ozdobie
+          Widzę i opisuję, bo tęsknię po tobie"
+          Panno święta, co Jasnej bronisz Częstochowy
+          I w Ostrej świecisz Bramie! Ty, co gród zamkowy
+          Nowogródzki ochraniasz z jego wiernym ludem!
+          Jak mnie dziecko do zdrowia powróciłaś cudem,
+          (Gdy od płaczącej matki pod Twoją opiekę
+          Ofiarowany, martwą podniosłem powiekę
+          I zaraz mogłem pieszo do Twych świątyń progu
+          Iść za wrócone życie podziękować Bogu),
+          Tak nas powrócisz cudem na Ojczyzny łono.
+          Tymczasem przenoś moją duszę utęsknioną
+          Do tych pagórków leśnych, do tych łąk zielonych,
+          Szeroko nad błękitnym Niemnem rozciągnionych;
+          Do tych pól malowanych zbożem rozmaitem,
+          Wyzłacanych pszenicą, posrebrzanych żytem;
+          Gdzie bursztynowy świerzop, gryka jak śnieg biała,
+          Gdzie panieńskim rumieńcem dzięcielina pała,
+          A wszystko przepasane jakby wstęgą, miedzą
+          Zieloną, na niej z rzadka ciche grusze siedzą.`)
+          }
+          color="primary"
+        >
+          Translate
+        </Button>
         <Button onClick={handleClose} color="primary">
           Cancel
         </Button>
-        <Button onClick={handleClose} color="primary">
+        <Button onClick={handleClose} color="primary" disabled={!translated}>
           Add
         </Button>
       </DialogActions>
