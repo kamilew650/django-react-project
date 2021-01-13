1bd002c734dd612ac484736988aa99e5cfd55507
Jarosław Niesyto (jaronie711) <jaronie711@student.polsl.pl>
2021-01-13 20:13:39 +0100

    Basic translation handler

--- client/src/components/CreateCardDialog/CreateCardDialog.js
+++ client/src/components/CreateCardDialog/CreateCardDialog.js
@@ -15,6 +15,24 @@ export default function CreateCardDialog(props) {
     props.setOpen(false);
   };
 
+  const translateHandler = async () => {
+    fetch(
+      "https://translation.googleapis.com/language/translate/v2?" +
+        new URLSearchParams({
+          key: process.env.REACT_APP_GOOGLE_API_KEY,
+          q: toTranslate,
+          target: "en",
+        }),
+      {
+        method: "post",
+      }
+    )
+      .then((res) => res.json(res))
+      .then((fetched) =>
+        setTranslated(fetched.data?.translations[0]?.translatedText)
+      );
+  };
+
   return (
     <Dialog
       open={props.open}
@@ -38,34 +56,7 @@ export default function CreateCardDialog(props) {
         <TextField disabled label="Translated" value={translated} fullWidth />
       </DialogContent>
       <DialogActions>
-        <Button
-          onClick={() =>
-            console.log(`
-          Litwo! Ojczyzno maja! Ty jesteś jak zdrowie,
-          Ile cię trzeba cenić, ten tylko się dowie,
-          Kto cię stracił. Dziś piękność twą w całej ozdobie
-          Widzę i opisuję, bo tęsknię po tobie"
-          Panno święta, co Jasnej bronisz Częstochowy
-          I w Ostrej świecisz Bramie! Ty, co gród zamkowy
-          Nowogródzki ochraniasz z jego wiernym ludem!
-          Jak mnie dziecko do zdrowia powróciłaś cudem,
-          (Gdy od płaczącej matki pod Twoją opiekę
-          Ofiarowany, martwą podniosłem powiekę
-          I zaraz mogłem pieszo do Twych świątyń progu
-          Iść za wrócone życie podziękować Bogu),
-          Tak nas powrócisz cudem na Ojczyzny łono.
-          Tymczasem przenoś moją duszę utęsknioną
-          Do tych pagórków leśnych, do tych łąk zielonych,
-          Szeroko nad błękitnym Niemnem rozciągnionych;
-          Do tych pól malowanych zbożem rozmaitem,
-          Wyzłacanych pszenicą, posrebrzanych żytem;
-          Gdzie bursztynowy świerzop, gryka jak śnieg biała,
-          Gdzie panieńskim rumieńcem dzięcielina pała,
-          A wszystko przepasane jakby wstęgą, miedzą
-          Zieloną, na niej z rzadka ciche grusze siedzą.`)
-          }
-          color="primary"
-        >
+        <Button onClick={() => translateHandler()} color="primary">
           Translate
         </Button>
         <Button onClick={handleClose} color="primary">
