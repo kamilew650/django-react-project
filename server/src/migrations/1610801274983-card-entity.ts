3c929d732239d0a4d8e5bbcfd03145b4d6db605e
kamilew650 <kamil.lewandowski1324@gmail.com>
2021-01-16 18:06:30 +0100

    fix(backend): migration update

--- server/src/migrations/1610801274983-card-entity.ts
+++ server/src/migrations/1610801274983-card-entity.ts
@@ -4,7 +4,7 @@ export class cardEntity1610801274983 implements MigrationInterface {
     name = 'cardEntity1610801274983'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "card" ("id" SERIAL NOT NULL, "orginal_content" character varying NOT NULL, "translated_content" character varying NOT NULL, CONSTRAINT "PK_9451069b6f1199730791a7f4ae4" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "card" ("id" SERIAL NOT NULL, "original_content" character varying NOT NULL, "translated_content" character varying NOT NULL, CONSTRAINT "PK_9451069b6f1199730791a7f4ae4" PRIMARY KEY ("id"))`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
